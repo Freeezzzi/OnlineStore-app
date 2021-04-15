ab5993e18276ff210799f1d00f74a9bdc8bba5e2
Freeezzzi <60979041+Freeezzzi@users.noreply.github.com>
2021-04-15 13:45:42 +0300

    добавил xml разметку для продуктов в листах

--- app/src/main/java/ru/freeezzzi/coursework/onlinestore/domain/models/User.kt
+++ app/src/main/java/ru/freeezzzi/coursework/onlinestore/domain/models/User.kt
@@ -3,6 +3,6 @@ package ru.freeezzzi.coursework.onlinestore.domain.models
 data class User(
     val username: String,
     val token: String,
-    var name: String,
-    var address: Address?
+    var name: String
+    //var address: Address?
 )
